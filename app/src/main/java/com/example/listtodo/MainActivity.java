a522e79804870e8fe670cf6dc1fd4eb96565ecbd
Gladyr <gladyrandrew@gmail.com>
2023-01-09 16:39:33 +0200

    singleton part2

--- app/src/main/java/com/example/listtodo/MainActivity.java
+++ app/src/main/java/com/example/listtodo/MainActivity.java
@@ -45,14 +45,19 @@ public class MainActivity extends AppCompatActivity {
         buttonAdNote = findViewById(R.id.buttonAdNote);
     }
 
-
-
     private void showNotes(){
         linearLayoutNotes.removeAllViews();
         for (Note note: dataBase.getNotes()
              ) {
             View view = getLayoutInflater().inflate(R.layout.note_item, linearLayoutNotes,
                     false);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dataBase.remove(note.getId());
+                    showNotes();
+                }
+            });
             TextView textViewNote = view.findViewById(R.id.textViewNote);
             textViewNote.setText(note.getTitle());
             linearLayoutNotes.addView(view);
